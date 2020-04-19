f40e1d48c018ee52986d6ad6d9ac14299a30a0c0
Claudio Busatto <cjcbusatto@gmail.com>
2020-04-19 12:37:16 +0200

    feat: include GET /transactions

--- src/routes/transactions.routes.ts
+++ src/routes/transactions.routes.ts
@@ -1,17 +1,20 @@
 import { Router } from 'express';
 import CreateTransactionService from '../services/CreateTransactionService';
-// import TransactionsRepository from '../repositories/TransactionsRepository';
 
-// import TransactionsRepository from '../repositories/TransactionsRepository';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+import { getCustomRepository } from 'typeorm';
 // import CreateTransactionService from '../services/CreateTransactionService';
 // import DeleteTransactionService from '../services/DeleteTransactionService';
 // import ImportTransactionsService from '../services/ImportTransactionsService';
 
 const transactionsRouter = Router();
 
-// transactionsRouter.get('/', async (request, response) => {
-//   // TODO
-// });
+transactionsRouter.get('/', async (request, response) => {
+  const transactionsRepository = getCustomRepository(TransactionsRepository);
+  const transactions = await transactionsRepository.find();
+
+  response.json(transactions);
+});
 
 transactionsRouter.post('/', async (request, response) => {
   const { title, value, type, category } = request.body;
